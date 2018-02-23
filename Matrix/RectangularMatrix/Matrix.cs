5a62f11aacdf8a4898b04fc0eed4e20420d96124
manestep <manestep@gmail.com>
2018-02-23 22:58:51 +0400

    add new sln and proj :two:

--- Matrix/RectangularMatrix/Matrix.cs
+++ Matrix/RectangularMatrix/Matrix.cs
@@ -5,14 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MatrixOperations
-{ /// <summary>
-/// NxM (N by M) Matrix
-/// </summary>
+{   
+    /// <summary>
+    /// NxM (N by M) Matrix
+    /// </summary>
     class Matrix
     {   // Number of matrix's rown
         public readonly int Row;
         // Number of matrix's columns
-        public readonly int Column; 
+        public readonly int Column;
         // Elements of Matrix
         public double[,] data;
         public double this[int i, int j]
@@ -26,17 +27,17 @@ namespace MatrixOperations
                 data[i, j] = value;
             }
         } /// <summary>
-        /// Constructing new matrix
-        /// </summary>
-        /// <param name="row"> Number of rows</param>
-        /// <param name="column">Number of columns</param>
+          /// Constructing new matrix
+          /// </summary>
+          /// <param name="row"> Number of rows</param>
+          /// <param name="column">Number of columns</param>
         public Matrix(int row = 0, int column = 0)
         {
             this.Row = row;
             this.Column = column;
             data = new double[row, column];
 
-        } 
+        }
         /// <summary>
         ///  Construct matrix by given array
         /// </summary>
@@ -118,14 +119,16 @@ namespace MatrixOperations
                 {
                     this.data[i, j] = scalar * data[i, j];
                 }
-            } return this;
-        } 
+            }
+            return this;
+        }
         /// <summary>
         /// Finding the smallest element of the matrix
         /// </summary>
         /// <returns> Smallest element</returns>
         public double SmallestElement()
-        { double min = this.data[0, 0];
+        {
+            double min = this.data[0, 0];
             for (int i = 0; i < this.Row; i++)
             {
                 for (int j = 0; j < this.Column; j++)
@@ -133,7 +136,8 @@ namespace MatrixOperations
                     if (this.data[i, j] < min)
                         min = data[i, j];
                 }
-            } return min;
+            }
+            return min;
         }
         /// <summary>
         /// Finding the largest element of the matrix
@@ -164,14 +168,15 @@ namespace MatrixOperations
                 {
                     Transpose[j, i] = data[i, j];
                 }
-            } return Transpose;
+            }
+            return Transpose;
         }
         /// <summary>
         ///  Creating an identity matrix
         /// </summary>
         /// <param name="N"> Number of rows and columns </param>
         /// <returns></returns>
-        public  Matrix  Identity(int N)
+        public Matrix Identity(int N)
         {
             Matrix I = new Matrix(N, N);
             for (int i = 0; i < N; i++)
@@ -184,12 +189,13 @@ namespace MatrixOperations
         /// <param name="matrix"> Matrix </param>
         /// <returns> Matrix's row-echelon form </returns>
         public Matrix BringMatrixToRowEchelonForm(Matrix matrix)
-        {   int n = matrix.Row;
+        {
+            int n = matrix.Row;
             Matrix I = Identity(matrix.Row);
             // Forming the block matrix using our matrix and identity matrix I
-            Matrix NewMatrix = new Matrix(n,2*n);
-           // Assigning to NewMatrix(nx2n) elements of our given matrix(nxn)
-            for (int numberOfRows= 0; numberOfRows < n; numberOfRows++)
+            Matrix NewMatrix = new Matrix(n, 2 * n);
+            // Assigning to NewMatrix(nx2n) elements of our given matrix(nxn)
+            for (int numberOfRows = 0; numberOfRows < n; numberOfRows++)
             {
                 for (int numberOfColumns = 0; numberOfColumns < n; numberOfColumns++)
                     NewMatrix[numberOfRows, numberOfColumns] = matrix[numberOfRows, numberOfColumns];
@@ -202,7 +208,7 @@ namespace MatrixOperations
                     NewMatrix[numberOfRows, newNumberOfColumns] = I[numberOfRows, newNumberOfColumns - n];
                 }
             }
-            int rowIndex = 0; int columnIndex = 0;           
+            int rowIndex = 0; int columnIndex = 0;
             while (rowIndex < n && columnIndex < n)
             {
                 // if matrix[i,j] is a pivot eliminate all non-zero entries below the pivot
@@ -242,12 +248,14 @@ namespace MatrixOperations
             }
             return NewMatrix;
 
-        } /// <summary>
+        } 
+        /// <summary>
         /// Bringing matrix to reduced row-echelon form(rref) and getting the inverse
         /// </summary>
         /// <returns> Inverse matrix</returns>
         public Matrix Inverse()
-        { if (this.Row != this.Column) throw new Exception("Matrix is not invertible as it is not square");
+        {
+            if (this.Row != this.Column) throw new Exception("Matrix is not invertible as it is not square");
             Matrix NewMatrix = BringMatrixToRowEchelonForm(this);
             int Rank = this.Row;
             int Count;
@@ -287,21 +295,23 @@ namespace MatrixOperations
                 }
             }
             // Assigning reverse matrix 
-             for (int i = 0; i < Row; i++)
+            for (int i = 0; i < Row; i++)
             {
                 for (int j = Row; j < 2 * Row; j++)
                 {
-                    this[i,j-Row]=NewMatrix[i,j];
+                    this[i, j - Row] = NewMatrix[i, j];
 
                 }
-            } return this;
+            }
+            return this;
         }
         /// <summary>
         /// Is matrix orthogonal
         /// </summary>
         /// <returns></returns>
         public bool IsOrthogonal()
-        { if (this.Row != this.Column) throw new Exception("Matrix is not square");
+        {
+            if (this.Row != this.Column) throw new Exception("Matrix is not square");
             if (this.Inverse() == this.Transpose()) return true;
             else return false;
         }
@@ -311,13 +321,14 @@ namespace MatrixOperations
         /// <param name="angle"> rotate vector by an angle  </param>
         /// <param name="vector"> vector to be rotated </param>
         public static void RotateVectorAboutX(double angle, Vector vector)
-        { double[,] Rotation = new double[,] { { 1, 0, 0 }, { 0, Math.Floor(Math.Cos(angle)), -Math.Floor(Math.Sin(angle)) }, { 0, Math.Floor(Math.Sin(angle)), Math.Floor(Math.Cos(angle)) } };
-            Matrix RotationMatrix = new Matrix(Rotation);               
+        {
+            double[,] Rotation = new double[,] { { 1, 0, 0 }, { 0, Math.Floor(Math.Cos(angle)), -Math.Floor(Math.Sin(angle)) }, { 0, Math.Floor(Math.Sin(angle)), Math.Floor(Math.Cos(angle)) } };
+            Matrix RotationMatrix = new Matrix(Rotation);
             Matrix VectorMatrix = new Matrix(3, 1);
             VectorMatrix[0, 0] = vector.x;
             VectorMatrix[1, 0] = vector.y;
             VectorMatrix[2, 0] = vector.z;
-            Matrix Result= RotationMatrix * VectorMatrix;
+            Matrix Result = RotationMatrix * VectorMatrix;
             Console.WriteLine("X =" + Result[0, 0] + " ");
             Console.WriteLine("Y =" + Result[1, 0] + " ");
             Console.WriteLine("Z =" + Result[2, 0] + " ");
@@ -330,7 +341,7 @@ namespace MatrixOperations
         /// <param name="vector"> vector to be rotated </param>
         public static void RotateVectorAboutY(double angle, Vector vector)
         {
-            double[,] Rotation = new double[,] { { Math.Floor(Math.Cos(angle)), -Math.Floor(Math.Sin(angle)), 0 }, { 0,1, 0 }, { -Math.Floor(Math.Sin(angle)), 0, Math.Floor(Math.Cos(angle)) } };
+            double[,] Rotation = new double[,] { { Math.Floor(Math.Cos(angle)), -Math.Floor(Math.Sin(angle)), 0 }, { 0, 1, 0 }, { -Math.Floor(Math.Sin(angle)), 0, Math.Floor(Math.Cos(angle)) } };
             Matrix RotationMatrix = new Matrix(Rotation);
             Matrix vectorMatrix = new Matrix(3, 1);
             vectorMatrix[0, 0] = vector.x;
@@ -350,7 +361,7 @@ namespace MatrixOperations
         public static void RotateVectorAboutZ(double angle, Vector vector)
         {
             double[,] Rotation = new double[,] { { Math.Floor(Math.Cos(angle)), -Math.Floor(Math.Sin(angle)), 0 }, { Math.Floor(Math.Sin(angle)), Math.Floor(Math.Cos(angle)), 0 }, { 0, 0, 1 } };
-            Matrix RotationMatrix = new Matrix(Rotation);           
+            Matrix RotationMatrix = new Matrix(Rotation);
             Matrix vectorMatrix = new Matrix(3, 1);
             vectorMatrix[0, 0] = vector.x;
             vectorMatrix[1, 0] = vector.y;
@@ -368,30 +379,30 @@ namespace MatrixOperations
         /// <param name="ScalingFactorZ">Factor for scaling about Z </param>
         /// <param name="vector"> Vector to be scaled </param>
         public static void Scaling(double ScalingFactorX, double ScalingFactorY, double ScalingFactorZ, Vector vector)
-        { 
+        {
             double[,] scaling = new double[,] { { ScalingFactorX, 0, 0 }, { 0, ScalingFactorY, 0 }, { 0, 0, ScalingFactorZ } };
             Matrix VectorMatrix = new Matrix(3, 1);
             Matrix ScalingMatrix = new Matrix(scaling);
-            VectorMatrix[0,0] = vector.x;
-            VectorMatrix[1,0] = vector.y;
-            VectorMatrix[2,0] = vector.z;
-                    
+            VectorMatrix[0, 0] = vector.x;
+            VectorMatrix[1, 0] = vector.y;
+            VectorMatrix[2, 0] = vector.z;
+
             Matrix result = ScalingMatrix * VectorMatrix;
-            Console.WriteLine("Scaling vector in the X direction by " + ScalingFactorX+ ":"+ result[0,0]);
+            Console.WriteLine("Scaling vector in the X direction by " + ScalingFactorX + ":" + result[0, 0]);
             Console.WriteLine("Scaling vector in the Y direction by " + ScalingFactorY + ":" + result[1, 0]);
             Console.WriteLine("Scaling vector in the Z direction by " + ScalingFactorX + ":" + result[2, 0]);
-            
+
         }
-        
-      
-       
-         /// <summary>
-          /// Translate the vector in the X, Y, Z direction
-          /// </summary>
-          /// <param name="x">Value that we want to add to X position</param>
-          /// <param name="y">Value that we want to add to y position</param>
-          /// <param name="z">Value that we want to add to Z position</param>
-          /// <param name="vector"> Vector</param>
+
+
+
+        /// <summary>
+        /// Translate the vector in the X, Y, Z direction
+        /// </summary>
+        /// <param name="x">Value that we want to add to X position</param>
+        /// <param name="y">Value that we want to add to y position</param>
+        /// <param name="z">Value that we want to add to Z position</param>
+        /// <param name="vector"> Vector</param>
         public static void Translation(double x, double y, double z, Vector vector)
         {
             Matrix matrix = new Matrix(4, 4);
@@ -411,7 +422,7 @@ namespace MatrixOperations
             matrix[1, 3] = y;
             matrix[2, 3] = z;
             matrix[3, 3] = 1;
-            Matrix VectorMatrix = new Matrix(4,1);
+            Matrix VectorMatrix = new Matrix(4, 1);
             VectorMatrix[0, 0] = vector.x;
             VectorMatrix[1, 0] = vector.y;
             VectorMatrix[2, 0] = vector.z;
@@ -427,13 +438,13 @@ namespace MatrixOperations
 
         public void Print()
         {
-            for(int i=0; i<Row; i++)
+            for (int i = 0; i < Row; i++)
             {
-                for(int j=0; j<Column; j++)
+                for (int j = 0; j < Column; j++)
                 {
                     Console.Write(data[i, j] + " ");
                 }
-                Console.WriteLine(" "); 
+                Console.WriteLine(" ");
             }
         }
 
--- Matrix/RectangularMatrix/Program.cs
+++ Matrix/RectangularMatrix/Program.cs
@@ -9,11 +9,12 @@ namespace MatrixOperations
     class Program
     {
         static void Main(string[] args)
-        { double[,]arr = new double[3,3] { { 1,2,3 }, { 0,1,4},{ 5,6,0 } };
+        {
+            double[,] arr = new double[3, 3] { { 1, 2, 3 }, { 0, 1, 4 }, { 5, 6, 0 } };
             Matrix m = new Matrix(arr);
-              Matrix m1 = new Matrix(3, 3);
-             double[,] arr1 = new double[3, 3] { { 0, 1 ,1}, { 2, -1 ,1}, {1, -1, -1} };
-              m1.data = arr1;
+            Matrix m1 = new Matrix(3, 3);
+            double[,] arr1 = new double[3, 3] { { 0, 1, 1 }, { 2, -1, 1 }, { 1, -1, -1 } };
+            m1.data = arr1;
             //  Matrix temp = m + m1;
             //  temp.Print();
             // Matrix forInverse = new Matrix(3, 3);
@@ -30,18 +31,18 @@ namespace MatrixOperations
             //  forInverse.Print();
             // bool b=  forInverse.IsOrthogonal();
             // Console.WriteLine(b);
-             Vector v = new Vector(1d, 2d, 6d);
+            Vector v = new Vector(1d, 2d, 6d);
 
             //     Matrix.RotateVectorAboutY((Math.PI)/2d, v);
             //    Matrix.ScalingByKthCoordinateAboutX(2, 3.0, v);
             //  Matrix.Translation(1, 0, 4 ,v);
             //  m1.Inverse();
             //   m1.Print();
-          //  Matrix.Scaling(3, 3, 3, v);
-           // Matrix.Translation(2, 1, 2, v);
+            //  Matrix.Scaling(3, 3, 3, v);
+            // Matrix.Translation(2, 1, 2, v);
             Matrix.RotateVectorAboutX(Math.PI / 2d, v);
-            
-          
+
+
         }
     }
 }

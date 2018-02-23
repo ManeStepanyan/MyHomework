69ed9c57dce4f51c911d9cac0559e3e7a69b89ae
manestep <manestep@gmail.com>
2018-02-23 23:27:19 +0400

    ......................................

--- NorInchvorok/AsenqConsole/Program.cs
+++ NorInchvorok/AsenqConsole/Program.cs
@@ -11,6 +11,12 @@ namespace AsenqConsole
         static void Main(string[] args)
         {
             Console.WriteLine(DateTime.Now.TimeOfDay);
+            Console.WriteLine("Mane jan");
+            .//ete push chanem chi el ereva che
+            //inch che//chi eta qani push chka
+            // ba inch es uzum :D MANNNNNN JNNNNAaa/
+            jaaan://asum em nenc ara or inchvorban sln papken ira amen inchov chlini githubum ok
+            /// eee hognats em chem jokum, et nuyny chi inch sln-n jnjem git-icvonc uzum es ara bayc anhetacra visual studioic push anelov
         }
     }
 }
